95044d75a72866f9f473f88fbc3ada2927054664
flack <163582560+FlackCode@users.noreply.github.com>
2024-06-10 21:05:58 +0200

    Committed template!

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class='h-screen w-full flex justify-center items-center bg-black'>
+    <h1 class="text-4xl font-bold text-white">Hello <span class="text-green-600">Vue</span></h1>
+  </div>
+</template>
+
